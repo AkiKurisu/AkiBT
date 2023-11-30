482d7a2c448dc405e9052799edf9b02dfe1a0a46
AkiKurisu <2683987717@qq.com>
2023-11-30 13:09:56 +0800

    Update BehaviorTreeSO.cs
    Allow reinitialize

--- Runtime/Core/BehaviorTreeSO.cs
+++ Runtime/Core/BehaviorTreeSO.cs
@@ -2,103 +2,98 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Kurisu.AkiBT
 {
-    /// <summary>
-    /// Behavior Tree ScriptableObject, can only run manually.
-    /// </summary>
-    [CreateAssetMenu(fileName = "BehaviorTreeSO", menuName = "AkiBT/BehaviorTreeSO")]
-    public class BehaviorTreeSO : ScriptableObject, IBehaviorTree
-    {
-        [SerializeReference, HideInInspector]
-        protected Root root = new();
-        public Root Root => root;
-        Object IBehaviorTree._Object => this;
-        public List<SharedVariable> SharedVariables => sharedVariables;
-#if UNITY_EDITOR
-        [SerializeField, HideInInspector]
-        private List<GroupBlockData> blockData = new();
-        public List<GroupBlockData> BlockData => blockData;
-        [Multiline]
-        public string Description;
-#endif
-        [HideInInspector, SerializeReference]
-        protected List<SharedVariable> sharedVariables = new();
         /// <summary>
-        /// Whether behaviorTreeSO is initialized
+        /// Behavior Tree ScriptableObject, can only run manually.
         /// </summary>
-        /// <value></value>
+        [CreateAssetMenu(fileName = "BehaviorTreeSO", menuName = "AkiBT/BehaviorTreeSO")]
+        public class BehaviorTreeSO : ScriptableObject, IBehaviorTree
+        {
+                [SerializeReference, HideInInspector]
+                protected Root root = new();
+                public Root Root => root;
+                Object IBehaviorTree._Object => this;
+                public List<SharedVariable> SharedVariables => sharedVariables;
+#if UNITY_EDITOR
+                [SerializeField, HideInInspector]
+                private List<GroupBlockData> blockData = new();
+                public List<GroupBlockData> BlockData => blockData;
+                [Multiline]
+                public string Description;
+#endif
+                [HideInInspector, SerializeReference]
+                protected List<SharedVariable> sharedVariables = new();
+                /// <summary>
+                /// Whether behaviorTreeSO is initialized
+                /// </summary>
+                /// <value></value>
 #if AKIBT_REFLECTION
-        public bool IsInitialized { get; private set; }
+                public bool IsInitialized { get; private set; }
 #else
-        public bool IsInitialized=>true;
+public bool IsInitialized=>true;
 #endif
-        /// <summary>
-        /// Bind GameObject and Init behaviorTree through Awake and Start method
-        /// </summary>
-        /// <param name="gameObject"></param>
-        public void Init(GameObject gameObject)
-        {
+                /// <summary>
+                /// Bind GameObject and Init behaviorTree through Awake and Start method
+                /// </summary>
+                /// <param name="gameObject"></param>
+                public void Init(GameObject gameObject)
+                {
 #if AKIBT_REFLECTION
-            if (!IsInitialized)
-            {
-                Initialize();
-            }
+                        if (!IsInitialized)
+                        {
+                                Initialize();
+                        }
 #endif
-            this.MapGlobal();
-            root.Run(gameObject, this);
-            root.Awake();
-            root.Start();
-        }
-        /// <summary>
-        /// Update BehaviorTree externally
-        /// </summary>
-        public virtual Status Update()
-        {
-            root.PreUpdate();
-            var status = root.Update();
-            root.PostUpdate();
-            return status;
-        }
-        public virtual void Deserialize(string serializedData)
-        {
-            var template = SerializeUtility.DeserializeTree(serializedData);
-            if (template == null) return;
-            root = template.Root;
-            sharedVariables = new List<SharedVariable>(template.Variables);
+                        this.MapGlobal();
+                        root.Run(gameObject, this);
+                        root.Awake();
+                        root.Start();
+                }
+                /// <summary>
+                /// Update BehaviorTree externally
+                /// </summary>
+                public virtual Status Update()
+                {
+                        root.PreUpdate();
+                        var status = root.Update();
+                        root.PostUpdate();
+                        return status;
+                }
+                public virtual void Deserialize(string serializedData)
+                {
+                        var template = SerializeUtility.DeserializeTree(serializedData);
+                        if (template == null) return;
+                        root = template.Root;
+                        sharedVariables = new List<SharedVariable>(template.Variables);
 #if UNITY_EDITOR
-            blockData = new List<GroupBlockData>(template.BlockData);
+                        blockData = new List<GroupBlockData>(template.BlockData);
 #endif
 #if AKIBT_REFLECTION
-            IsInitialized = true;
-            SharedVariableMapper.Traverse(this);
+                        IsInitialized = true;
+                        SharedVariableMapper.Traverse(this);
 #endif
-        }
+                }
 #if AKIBT_REFLECTION
-        /// <summary>
-        /// This will be called when the object is loaded for the first time when entering PlayMode
-        /// Currently we only need to map variables once per scriptableObject
-        /// </summary>
-        private void Awake()
-        {
-            if (!IsInitialized)
-            {
-                Initialize();
-            }
-        }
+                /// <summary>
+                /// This will be called when the object is loaded for the first time when entering PlayMode
+                /// Currently we only need to map variables once per scriptableObject
+                /// </summary>
+                private void Awake()
+                {
+                        if (!IsInitialized)
+                        {
+                                Initialize();
+                        }
+                }
 #endif
-        /// <summary>
-        /// Traverse behaviors and bind shared variables
-        /// </summary>
-        public void Initialize()
-        {
+                /// <summary>
+                /// Traverse behaviors and bind shared variables
+                /// </summary>
+                public void Initialize()
+                {
 #if AKIBT_REFLECTION
-            if (IsInitialized)
-            {
-                Debug.LogWarning($"{name} initialize duplicate which should not occur");
-                return;
-            }
-            IsInitialized = true;
-            SharedVariableMapper.Traverse(this);
+                        IsInitialized = true;
+                        SharedVariableMapper.Traverse(this);
 #endif
+                }
         }
-    }
 }
